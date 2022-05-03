4f21678b3bb2cb901e3446881dc4885034836ba2
Soso-C <sonny.capisciolti@gmail.com>
2022-05-03 23:23:20 +0200

    update navbar + add newCard

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import logoMain from "../../assets/ressources/logo_awake/SVG/logo5.svg";
 import logoMenuBurger from "../../assets/ressources/menu.svg";
@@ -8,18 +8,10 @@ const Navbar = () => {
   // Target for switch bg navbar if we're at homepage
   const location = useLocation();
 
-  // Toggle navbar burgermenu for mobile
-  const toggleNav = () => {
-    const menuEltToggle = document.querySelectorAll(".toggle-menu");
-    menuEltToggle.forEach((el) => el.classList.toggle("hidden"));
-  };
+  const [toggle, setToggle] = useState(false);
 
-  // It's for mobile people when an user click on navbar links then close this one
-  const closeNav = () => {
-    const menuEltToggle = document.querySelectorAll(".toggle-menu");
-    if (menuEltToggle.classList !== "hidden") {
-      menuEltToggle.forEach((el) => el.classList.add("hidden"));
-    }
+  const showNav = () => {
+    setToggle(!toggle);
   };
 
   return (
@@ -48,17 +40,21 @@ const Navbar = () => {
 
           <button
             className="inline-flex items-center justify-center rounded-md bg-white p-2 ring-1 ring-black ring-opacity-20 md:hidden"
-            onClick={toggleNav}
+            onClick={showNav}
           >
             <img src={logoMenuBurger} alt="icone burger" className="h-6 w-6" />
           </button>
         </div>
 
         {/* <!-- List link navbar --> */}
-        <ul className="toggle-menu flex hidden flex-col bg-[#232323] px-1 pt-6 md:flex md:w-auto md:flex-row md:items-center md:justify-center md:space-x-5 md:bg-transparent md:pt-0 lg:space-x-8">
+        <ul
+          className={`${
+            toggle ? " flex" : " hidden"
+          } flex-col bg-[#232323] px-1 pt-6 md:flex md:w-auto md:flex-row md:items-center md:justify-center md:space-x-5 md:bg-transparent md:pt-0 lg:space-x-8`}
+        >
           <li className="group border-t border-gray-200 py-2 text-center md:border-0">
             <NavLink
-              onClick={closeNav}
+              onClick={showNav}
               to="/"
               className="text-lg md:text-base lg:text-lg font-medium text-white group-hover:text-sky-500"
             >
@@ -73,7 +69,7 @@ const Navbar = () => {
           </li>
           <li className="group border-t border-gray-200 py-2 text-center md:border-0">
             <NavLink
-              onClick={closeNav}
+              onClick={showNav}
               to="/ranks"
               className="text-lg md:text-base lg:text-lg font-medium text-white group-hover:text-sky-500"
             >
@@ -94,7 +90,11 @@ const Navbar = () => {
           </li>
         </ul>
         {/* <!-- Btn Link Account --> */}
-        <div className="toggle-menu mt-6 flex hidden items-center justify-center md:mt-0 md:flex">
+        <div
+          className={`${
+            toggle ? " flex" : " hidden"
+          } mt-6 items-center justify-center md:mt-0 md:flex`}
+        >
           <Link
             to="/"
             className="px-[12px] py-[6px] rounded-md border bg-white text-center font-medium text-sky-700 shadow-md hover:bg-gray-300"
